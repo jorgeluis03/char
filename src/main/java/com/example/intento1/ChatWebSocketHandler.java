33a00d3ea6f61c8aa6ff1d821bfa4fec589b0a8f
jorgeluis <jorgel.dominguez03@gmail.com>
2023-07-01 10:38:29 -0500

    chat mejorado V2

--- src/main/java/com/example/intento1/ChatWebSocketHandler.java
+++ src/main/java/com/example/intento1/ChatWebSocketHandler.java
@@ -2,9 +2,7 @@ package com.example.intento1;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.*;
-
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -18,18 +16,15 @@ public class ChatWebSocketHandler implements WebSocketHandler {
 
     /*Se ejecuta cuando se establece una nueva coneccion webSocket
       se agrega la sesion del cliente a la lista de sesiones activas*/
-    int i=1;
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
         sessions.add(session);
-        System.out.println("El Id de la sesion "+i+" es: "+session.getId());
-        i++;
     }
-
 
     /*se invoca cuando se recibe un mensaje WebSocket de un cliente
       En este ejemplo, el mensaje se reenvía a todas las sesiones activas,
       lo que permite la mensajería entre los clientes conectados*/
+
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
         // Obtener el mensaje enviado por el cliente
@@ -38,7 +33,7 @@ public class ChatWebSocketHandler implements WebSocketHandler {
         if (messageText.startsWith("OPEN:")) {
 
             // Este mensaje proviene del evento socket.onopen
-            System.out.println("Nombre es: " + messageText+" con ID: "+session.getId());
+            System.out.println("Nombre es: " + messageText+" con IDSession: "+session.getId());
             sesion_idSesion.put(session, session.getId());
             idSesion_username.put(session.getId(),messageText);
 
@@ -47,53 +42,52 @@ public class ChatWebSocketHandler implements WebSocketHandler {
                 s.sendMessage(new TextMessage("OPEN:"+session.getId()+":"+messageText.substring(5)));
             }
 
-            //Envia todas la sesiones ya existente a la sesion actual
-            for(String usuarioConect : sesion_idSesion.values()){
-                //omite volver a enviar la sesion actual dos veces
-                if(!session.getId().equals(usuarioConect)){
-                    session.sendMessage(new TextMessage("OPEN:"+usuarioConect+":chat"+i));
-                    i = i+1;
-                }//intentar hacerlo con un hashmap de sessiongetID y Nombres
-            }
-            for(String user : idSesion_username.values()){
-                System.out.println("Hashmap: "+user);
-            }
+            // Enviar todas las sesiones ya existentes a la sesión actual
+            for (Map.Entry<WebSocketSession, String> entry : sesion_idSesion.entrySet()) {
+                WebSocketSession existingSession = entry.getKey();
+                String sessionId = entry.getValue();
 
+                if (!session.getId().equals(sessionId) && !messageText.equals(idSesion_username.get(sessionId))) {
+                    session.sendMessage(new TextMessage("OPEN:" + sessionId + ":"+idSesion_username.get(sessionId).substring(5)));
+                    System.out.println("Hashmap: " + idSesion_username.get(sessionId));
+                }
+            }
         } else if (messageText.startsWith("CLICK:")) {
             // Este mensaje proviene del evento $("#enviar").click(function ()
-            System.out.println("Contenido del mensaje por 'enviar': " + messageText);
+            System.out.println("Contenido del mensaje enviado: " + messageText);
 
             /*se divide el mensaje en partes utilizando el carácter ":" como separador.
                 El número 3 indica que se dividirá el mensaje en máximo 4 partes. */
             String[] parts = messageText.split(":", 4);
-            /* parts[0] contendrá "CLICK", parts[1] contendrá el destinatario y parts[2] contendrá el remitente, parts[3] el contenido del mensaje.*/
 
-            String destinatario = "OPEN:"+parts[1];
+            /* parts[0] contendrá "CLICK", parts[1] contendrá el destinatario y parts[2] contendrá el remitente, parts[3] el contenido del mensaje.*/
+            String destinatario = parts[1];
             String remitente = parts[2];
             String contenido = parts[3];
+            System.out.println("el remitente es: "+remitente);
             System.out.println("el destinaraios es: "+destinatario);
+            System.out.println("el contenido es: "+contenido);    //cotinuat con el envio de emnsajes
 
-            if (destinatario.equals("OPEN:todos")) {
+            if (destinatario.equals("todos")) {
                 // Enviar el mensaje a todos los clientes
                 for (WebSocketSession s : sessions) {
                     s.sendMessage(new TextMessage("CLICK:"+remitente+": "+contenido));
                 }
             }else {
                 // Encontrar la sesión del destinatario y enviar el mensaje solo a esa sesión
-                for (WebSocketSession s : sessions) {
-                    if (destinatario.equals(sesion_idSesion.get(s))) {
-                        System.out.println("la sesion encontrada es: "+sesion_idSesion.get(s));
-                        s.sendMessage(new TextMessage("CLICK:"+remitente+": "+contenido));
-                        session.sendMessage(new TextMessage("CLICK:Tú: "+contenido));
+                for (Map.Entry<WebSocketSession, String> entry : sesion_idSesion.entrySet()) {
+                    WebSocketSession existingSession = entry.getKey();
+                    String sessionId = entry.getValue();
+                    if (destinatario.equals(sessionId)) {
+                        System.out.println("La sesión encontrada es: " + sessionId);
+                        existingSession.sendMessage(new TextMessage("CLICK:" + remitente + ": " + contenido));
+                        session.sendMessage(new TextMessage("CLICK:Tú: " + contenido));
                     }
-
                 }
             }
 
         }
     }
-
-
 
     /*se ejecuta cuando se cierra una conexión WebSocket.
         Aquí, se elimina la sesión del cliente de la lista de sesiones activas*/
